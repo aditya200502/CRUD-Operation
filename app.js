22e9b4dc1ccaf960de5484d59d10c7ac8cca707b
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-11-08 18:45:45 +0530

    Database connectivity done

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = process.env.PORT || '8080'
+
+import connect from "./db/connect.js"
+
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://0.0.0.0:27017";
+
+//Database is connected :-
+connect(DATABASE_URL);
 
 app.listen(port , () => {
     console.log(`Server is responding at http://localhost:${port}`);
