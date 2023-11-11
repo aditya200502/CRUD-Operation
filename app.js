90c2e8edd490d9c7e5f19655a2a6ab97563185c4
Aditya Srivastava <93389230+aditya200502@users.noreply.github.com>
2023-11-11 09:41:42 +0530

    Create and Read section done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import {join} from "path";
+import express, { urlencoded } from "express";
+import { join } from "path";
 import connect from "./db/connect.js"
 import web from "./routes/web.js";
 
@@ -11,18 +11,20 @@ const port = process.env.PORT || '8080'
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://0.0.0.0:27017";
 
 //Static files enabiling :-
-app.use('/student',express.static(join(process.cwd(), "public")))
-app.use('/student/edit',express.static(join(process.cwd(), "public")))
+app.use('/student', express.static(join(process.cwd(), "public")))
+app.use('/student/edit', express.static(join(process.cwd(), "public")))
 
 //Set Templates Engine :-
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }))
+
 
 //Load Routes
-app.use("/student",web);
+app.use("/student", web);
 
 //Database is connected :-
 connect(DATABASE_URL);
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`Server is responding at http://localhost:${port}`);
 })
--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -2,29 +2,47 @@ import StudentModel from "../models/Student.js";
 
 class StudentController {
 
-    static createDoc = (req, res) => {
+    // Craeting  Document
+    static createDoc = async(req, res) => {
+        // console.log(req.body);
+        const {name,age,fees} = req.body;
+
+        //Save the field
+        const doc = new StudentModel({
+            name:name,
+            age:age,
+            fees:fees
+        })
+
+        const result = await doc.save()
+
         res.redirect("/student")
     }
+
+    // Retrieving Document
     static getAllDoc = async (req, res) => {
         try {
             const result = await StudentModel.find();
 
-            console.log(result);
+            //console.log(result);
             res.render("index", { result });
 
         } catch (err) {
             console.log(err);
         }
-    } 
+    }
 
+    // Editing Document
     static editDoc = (req, res) => {
         res.render("edit");
     }
 
+    // Updating document
     static updateDocById = (req, res) => {
         res.render("/student");
     }
 
+    // Deleting deleting
     static deleteDocById = (req, res) => {
         res.redirect("/student");
     }
